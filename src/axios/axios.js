e33ca1b3d280e9565a7e07fe914224122e6f2692
patrick-n4 <patrickndayambaje4@gmail.com>
2022-05-26 10:00:16 +0200

    I resolved this

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4040"
+  baseURL: "http://localhost:4040" || "https://patrick-first.herokuapp.com/"
 });
 
 export default instance;
