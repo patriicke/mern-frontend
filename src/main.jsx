af771d5153b89038196edb250124dc5963b779f3
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-02 22:10:37 +0200

    The core concept her to ber coverd tomorrow

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+//The core concept her to ber coverd tomorrow
 const container = document.querySelector("#root");
 const root = createRoot(container);
 root.render(<App />);
